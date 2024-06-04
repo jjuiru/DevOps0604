5ad7c2a17af7893f97de913296ad78aaf15a6c9f
jjuiru <shj00035@naver.com>
2024-06-04 09:53:20 +0900

    first commit

---
+++ src/main/java/com/mohaja/vs0604/Vs0604Application.java
@@ -0,0 +1,13 @@
+package com.mohaja.vs0604;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vs0604Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vs0604Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohaja/vs0604/Vs0604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohaja.vs0604;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vs0604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
